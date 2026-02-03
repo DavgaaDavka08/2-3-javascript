650258cfab5680c8feab5c404ba6236ea4ea62c8
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2026-02-03 12:08:30 +0800

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+let age = 25;
+
+//1 3 toonii dondaj
+
+//2 18 ih bol nasand hursen hun if - nohtsol shaga
+
+//3 2 odort heden tsag baih ve
+let day = 2;
+let hour = 24;
+//4 price 250000 hymdral 15 % hymdraltai
+
+let price = 100;
+let discount = 0.15;
+let discountAmount = price * discount; //
+console.log("discountAmount :>> ", discountAmount);
+let finaPrice = price - discountAmount;
+console.log("answer :>> ", finaPrice);
+// 15% hongolt gedeg ni 100-aas 15 hasna gesen ug tegheer 85% iig tolno
